be806ebda792e510abfb8a7938291b3483834b76
Kevyn silva <161781761+KevynSilvaD@users.noreply.github.com>
2024-09-25 11:23:52 -0300

    Update Mysql.java

--- text/src/repository/Mysql.java
+++ text/src/repository/Mysql.java
@@ -10,18 +10,15 @@ public class Mysql {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Obtendo as variáveis de ambiente
-            String dbUrl = System.getenv("DB_URL");
-            String dbUser = System.getenv("DB_USER");
-            String dbPassword = System.getenv("DB_PASSWORD");
-
-            // Estabelecendo a conexão
-            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/kevyn_silva",
+                    "kevyn_silva",
+                    "123"
+            );
             return connection;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-            System.out.println("Erro ao tentar conectar com o banco.");
+            System.out.println("Erro ao tentar connectar com o banco.");
         } catch (ClassNotFoundException e) {
             System.out.println("Erro ao tentar importar o driver mysql");
         }
