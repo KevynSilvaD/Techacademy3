a9ab1383d2a0922d27bfbc1cebf4bfc2cd9147b1
Kevyn silva <161781761+KevynSilvaD@users.noreply.github.com>
2024-09-25 19:13:31 -0300

    adicionei imagens

--- jogo.php
+++ jogo.php
@@ -1,9 +1,16 @@
 <?php include 'php/header.php'; ?>
 
-<!-- Conteúdo da página do jogo -->
-<main class="container my-5">
+<main class="container my-5 text-center">
     <h1 class="text-center">Jogue A Mansão da Névoa</h1>
-    <a href="http://localhost:4567">clique para jogar</a>
-</main>
 
+ 
+    <img src="imagens/4.jpeg" alt="investigador" class="img-fluid my-3" style="max-width: 450px;">
+
+    
+</main>
+<body>
+    <div class="container my-3 text-center">
+<a href="http://localhost:4567" class="btn btn-success btn-lg ">CLIQUE AQUI</a>
+</div>
+</body>
 <?php include 'php/footer.php'; ?>
